4619a9a4d7ce832554d2d57bd24f94d6aec6872c
darrell2604 <42405671+darrell2604@users.noreply.github.com>
2026-08-31 18:02:46 +0100

    Expose decomp adapter from runtime

--- android/app/src/main/cpp/game_runtime.h
+++ android/app/src/main/cpp/game_runtime.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "decomp_runtime_adapter.h"
 #include "platform_services.h"
 
 namespace open_crossing {
@@ -12,10 +13,12 @@ public:
 
     bool ready() const { return ready_; }
     const PlatformServices& platform() const { return platform_; }
+    const DecompRuntimeAdapter& decomp() const { return decomp_; }
 
 private:
     bool ready_ = false;
     PlatformServices platform_{};
+    DecompRuntimeAdapter decomp_{};
     int width_ = 0;
     int height_ = 0;
     unsigned long long frame_counter_ = 0;
