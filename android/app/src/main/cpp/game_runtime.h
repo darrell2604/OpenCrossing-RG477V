9fcbdb1d8c960cd62aa162e4efa2de8bc8d102ae
darrell2604 <42405671+darrell2604@users.noreply.github.com>
2026-08-31 20:40:36 +0100

    Integrate player camera and interaction runtime

--- android/app/src/main/cpp/game_runtime.h
+++ android/app/src/main/cpp/game_runtime.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 #include "decomp_game_loop.h"
 #include "decomp_runtime_adapter.h"
 #include "platform_services.h"
 #include "scene_state.h"
+#include "player_camera.h"
+#include "player_interaction.h"
 
 namespace open_crossing {
 
@@ -20,6 +23,8 @@ public:
     const DecompRuntimeAdapter& decomp() const { return decomp_; }
     const DecompGameLoop& game_loop() const { return game_loop_; }
     const SceneState& scene() const { return scene_; }
+    const PlayerCamera& camera() const { return camera_; }
+    const PlayerInteraction& interaction() const { return interaction_; }
 
 private:
     bool ready_ = false;
@@ -27,6 +32,9 @@ private:
     DecompRuntimeAdapter decomp_{};
     DecompGameLoop game_loop_{};
     SceneState scene_{};
+    PlayerCamera camera_{};
+    PlayerInteraction interaction_{};
+    std::vector<InteractionTarget> interaction_targets_;
     int width_ = 0;
     int height_ = 0;
     unsigned long long frame_counter_ = 0;
